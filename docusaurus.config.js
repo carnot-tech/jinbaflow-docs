0b9b4c0b60ce887ab7732d622e353bacbf65fb55
brian-carnot <brian.l@carnot.ai>
2024-03-19 15:15:25 +0900

    adjust back to jinbaflow button

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -78,7 +78,7 @@ const config = {
           },
           // {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://pf2.prompt-flow.com/login',
+            href: 'https://flow.jinba.ai/',
             label: 'Back to Jinbaflow',
             position: 'right',
           },
